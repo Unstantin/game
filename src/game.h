9e3fcb5610554673d500df8310e51fd839645975
Konstantin Samoylovich <samoylovich2005@gmail.com>
2022-02-08 23:31:19 +0300

    game.h added

---
+++ src/game.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef GAME
+#define GAME
+
+#define BOARD_N 7
+#define BOARD_M 7
+
+//#define GAME_DEV_MODE
+#define GAME_TITLE "Game"
+
+enum print_mode {PRINT_CHILD, PRINT_ROOT};
+
+#endif
